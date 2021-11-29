5c9ca2170e24c633594af41c0e1b8e3345859e9b
elepot87 <78643839+elepot87@users.noreply.github.com>
2021-11-29 12:41:50 +0100

    chiamata axios e visualizzazione dei film in pagina

---
+++ src/components/Card.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container-card">
+    <h4 class="title">{{ title }}</h4>
+    <h5 class="title-original">{{ subtitle }}</h5>
+    <div class="language">{{ info1 }}</div>
+    <div class="ratings">{{ info2 }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Card",
+  props: {
+    title: String,
+    subtitle: String,
+    info1: String,
+    info2: Number,
+  },
+};
+</script>
+
+<style scoped lang="scss">
+@import "@/styles/card";
+</style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,19 +1,55 @@
 <template>
   <main class="main-container">
     <ul class="container-film">
-      <li class="film-item">
-        <h4 class="title">Titolo</h4>
+      <li
+        class="film-item"
+        v-for="(film, index) in filmList"
+        :key="`film-${index}`"
+      >
+        <Card
+          :title="film.title"
+          :subtitle="film.original_title"
+          :info1="film.original_language"
+          :info2="film.vote_average"
+        />
+        <!-- <h4 class="title">Titolo</h4>
         <h5 class="title-original">Titolo originale</h5>
         <div class="language">Lingua</div>
-        <div class="ratings">Voto</div>
+        <div class="ratings">Voto</div> -->
       </li>
     </ul>
   </main>
 </template>
 
 <script>
+import axios from "axios";
+import Card from "@/components/Card.vue";
 export default {
   name: "Main",
+  components: {
+    Card,
+  },
+  data() {
+    return {
+      filmList: [],
+    };
+  },
+  created() {
+    this.getFilmList();
+  },
+  methods: {
+    getFilmList() {
+      axios
+        .get(
+          "https://api.themoviedb.org/3/search/movie/?api_key=694d4228c101f485c77573eb27c4523a&query=Fantozzi&language=it-IT"
+        )
+        .then((response) => {
+          console.log(response.data.results);
+          this.filmList = response.data.results;
+        })
+        .catch((err) => console.log(err));
+    },
+  },
 };
 </script>
 
