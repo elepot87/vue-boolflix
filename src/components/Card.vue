8b4bcd3844cbea0082426518e2c97bc4e2e712a1
elepot87 <78643839+elepot87@users.noreply.github.com>
2021-11-29 18:14:12 +0100

    aggiunte flag

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -2,7 +2,15 @@
   <div class="container-card">
     <h4 class="title">{{ title }}</h4>
     <h5 class="title-original">{{ subtitle }}</h5>
-    <div class="language">{{ info1 }}</div>
+    <div class="language">
+      <img
+        v-if="flag"
+        :src="require(`@/assets/img/${info1}.png`)"
+        alt=""
+        class="flag-lang"
+      />
+      <div class="language" v-else>Lingua: {{ info1 }}</div>
+    </div>
     <div class="ratings">{{ info2 }}</div>
   </div>
 </template>
@@ -15,6 +23,16 @@ export default {
     subtitle: String,
     info1: String,
     info2: Number,
+  },
+  data() {
+    return {
+      flags: ["en", "it"],
+    };
+  },
+  computed: {
+    flag() {
+      return this.flags.includes(this.info1);
+    },
   },
 };
 </script>
