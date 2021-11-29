01a9a3b5067fbf436455629f6bb1e68bde52a070
elepot87 <78643839+elepot87@users.noreply.github.com>
2021-11-29 11:54:34 +0100

    add header and main component

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <header class="header">
+    <form action="">
+      <input type="text" class="form-control" placeholder=">Search film" />
+      <button type="submit">Search</button>
+    </form>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style scoped lang="scss">
+@import "@/styles/header";
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,22 @@
+<template>
+  <main class="main-container">
+    <ul class="container-film">
+      <li class="film-item">
+        <h4>Titolo</h4>
+        <h5>Titolo originale</h5>
+        <div>Lingua</div>
+        <div>Voto</div>
+      </li>
+    </ul>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "Main",
+};
+</script>
+
+<style scoped lang="scss">
+@import "@/styles/main";
+</style>
