a8e2aa6763960e67e6f89dae44e93cfbeea05239
elepot87 <78643839+elepot87@users.noreply.github.com>
2021-11-29 11:57:14 +0100

    add some margin for film item

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -2,10 +2,10 @@
   <main class="main-container">
     <ul class="container-film">
       <li class="film-item">
-        <h4>Titolo</h4>
-        <h5>Titolo originale</h5>
-        <div>Lingua</div>
-        <div>Voto</div>
+        <h4 class="title">Titolo</h4>
+        <h5 class="title-original">Titolo originale</h5>
+        <div class="language">Lingua</div>
+        <div class="ratings">Voto</div>
       </li>
     </ul>
   </main>
