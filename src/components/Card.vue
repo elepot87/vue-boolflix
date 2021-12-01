bff810f3de18a566435c948d166a1ea82c90c4b2
elepot87 <eleonorapotente@gmail.com>
2021-12-01 19:03:41 +0100

    inizio stile per le card

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -5,30 +5,33 @@
         v-if="posterOk"
         :src="`https://image.tmdb.org/t/p/w185/${poster}`"
         alt=""
+        class="poster"
       />
       <div class="alternative-img" v-else>Immagine non disponibile</div>
     </div>
-
-    <h4 class="title">{{ title }}</h4>
-    <h5 class="title-original">{{ subtitle }}</h5>
-    <div class="language">
-      <img
-        v-if="flag"
-        :src="require(`@/assets/img/${info1}.png`)"
-        alt="info1"
-        class="flag-lang"
-      />
-      <div class="language" v-else>Lingua: {{ info1 }}</div>
+    <div class="container-info">
+      <h4 class="title">{{ title }}</h4>
+      <h5 class="title-original">{{ subtitle }}</h5>
+      <div class="language">
+        <img
+          v-if="flag"
+          :src="require(`@/assets/img/${info1}.png`)"
+          alt="info1"
+          class="flag-lang"
+        />
+        <div class="language" v-else>Lingua: {{ info1 }}</div>
+      </div>
+      <div class="ratings">
+        <i
+          class="fas fa-star"
+          v-for="n in getVoteStars(info2)"
+          :key="`stars-${n}`"
+        ></i>
+        <i class="far fa-star" v-for="n in 5 - getVoteStars(info2)" :key="n">
+        </i>
+      </div>
+      <div class="overview">{{ text }}</div>
     </div>
-    <div class="ratings">
-      <i
-        class="fas fa-star"
-        v-for="n in getVoteStars(info2)"
-        :key="`stars-${n}`"
-      ></i>
-      <i class="far fa-star" v-for="n in 5 - getVoteStars(info2)" :key="n"> </i>
-    </div>
-    <div class="overview">{{ text }}</div>
   </div>
 </template>
 
