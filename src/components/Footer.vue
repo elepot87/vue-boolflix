2cee833216633220a9df063741e27d6853b54e02
elepot87 <eleonorapotente@gmail.com>
2021-12-01 22:42:23 +0100

    stile footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,59 @@
 <template>
-  <footer>
-    <div>sono un footer</div>
+  <footer class="footer">
+    <div class="footer-high">
+      <a
+        href="https://www.facebook.com/verdepastelloadvsolutions/"
+        target="_blank"
+      >
+        <i class="fab fa-facebook-f icon-footer"></i>
+      </a>
+      <a href="https://www.instagram.com/verdepastello_to/" target="_blank">
+        <i class="fab fa-instagram icon-footer"></i>
+      </a>
+      <a
+        href="https://www.linkedin.com/company/verdepastello/ "
+        target="_blank"
+      >
+        <i class="fab fa-linkedin-in icon-footer"></i>
+      </a>
+    </div>
+    <div class="footer-low d-flex">
+      <div class="first">
+        <ul class="menu-footer">
+          <li><a href="#" class="link-footer">Audio e sottotitoli</a></li>
+          <li><a href="#" class="link-footer">Media Center</a></li>
+          <li><a href="#" class="link-footer">Privacy</a></li>
+          <li><a href="#" class="link-footer">Contattaci</a></li>
+        </ul>
+      </div>
+      <div class="second">
+        <ul class="menu-footer">
+          <li><a href="#" class="link-footer">Audiodescrizione</a></li>
+          <li>
+            <a href="#" class="link-footer">Rapporti con gli investitori</a>
+          </li>
+          <li><a href="#" class="link-footer">Note legali</a></li>
+        </ul>
+      </div>
+      <div class="third">
+        <ul class="menu-footer">
+          <li><a href="#" class="link-footer">Centro assistenza</a></li>
+          <li>
+            <a href="#" class="link-footer">Opportunità di lavoro</a>
+          </li>
+          <li><a href="#" class="link-footer">Preferenze per i cookie</a></li>
+        </ul>
+      </div>
+      <div class="fourth">
+        <ul class="menu-footer">
+          <li><a href="#" class="link-footer">Carte regalo</a></li>
+          <li>
+            <a href="#" class="link-footer">Condizioni di utilizzo</a>
+          </li>
+          <li><a href="#" class="link-footer">Informazioni sull'azienda</a></li>
+        </ul>
+      </div>
+    </div>
   </footer>
 </template>
 
